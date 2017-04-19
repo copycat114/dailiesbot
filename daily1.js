d6be7e7e07f90b8613db040993c0fc843eba2633
copycat114 <superman1762@gmail.com>
2017-04-19 14:35:56 -0400

    Initial Commit

---
+++ daily1.js
@@ -0,0 +1,25 @@
+/*
+  This bot will make a bot user give daily credits to a user
+*/
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+const token = config.Token1;
+
+function doexit() {
+  process.exit()
+};
+
+client.on('ready', () => {
+  console.log('TatsuBot 1 has started.');
+  const channel = client.channels.get(config.ChannelID);
+    channel.send("t!dailies <@" + config.OwnerID + ">");
+	channel.send("t!rep <@" + config.OwnerID + ">");
+	setTimeout(doexit, 3000);
+});
+
+client.login(token);
---
+++ runlinux.sh
@@ -0,0 +1,4 @@
+node ./daily1.js
+node ./daily2.js
+node ./daily3.js
+node ./daily4.js
